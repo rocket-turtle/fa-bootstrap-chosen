9f788cf9e46b60d18ecf3073ba07de426487e113
Jonas <jonas__s@web.de>
2016-04-03 13:11:15 +0200

    remove bootstrap from fa-bootstrap-chosen.scss file and make a new folder for the compass compile

--- config.rb
+++ config.rb
@@ -6,8 +6,8 @@ require 'compass/import-once/activate'
 # Set this to the root of your project when deployed:
 http_path = "/"
 css_dir = "public/stylesheets"
-sass_dir = "sass"
-images_dir = "images"
+sass_dir = "sass-for-demo"
+# images_dir = "images"
 # javascripts_dir = "javascripts"
 
 # You can select your preferred output style here (can be overridden via the command line):
